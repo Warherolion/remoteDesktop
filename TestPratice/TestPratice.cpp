2046e1009d518f87826801af702ce0116329b8b6
Ranvir Singh <Warherolion@users.noreply.github.com>
2024-02-13 13:25:10 -0500

    first commit

---
+++ TestPratice/TestPratice.cpp
@@ -0,0 +1,12 @@
+// TestPratice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <asio.hpp>
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!\n";
+
+
+
+}
+
